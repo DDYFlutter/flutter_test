77a9f9662812bbeb82eca4a3288f241b289cb826
rain <634778311@qq.com>
2019-09-09 17:02:14 +0800

    Create interface.js

---
+++ interface.js
@@ -0,0 +1,19 @@
+{   
+    "code":"200",
+    "data":
+    [{
+        "name"    : "Flutter Network1",
+        "content" : "1 Hello world! Hello Flutter! This is a test data for Flutter-Network-Request",
+        "image"  : "https://upload-images.jianshu.io/upload_images/1465510-3e574451110bf06b.png"
+    },
+    {
+        "name"    : "Flutter Network2",
+        "content" : "2 Hello world! Hello Flutter! This is a test data for Flutter-Network-Request",
+        "image"  : "https://upload-images.jianshu.io/upload_images/1465510-3e574451110bf06b.png"
+    },
+    {
+        "name"    : "Flutter Network2",
+        "content" : "3 Hello world! Hello Flutter! This is a test data for Flutter-Network-Request",
+        "image"  : "https://upload-images.jianshu.io/upload_images/1465510-3e574451110bf06b.png"
+    }]
+}
