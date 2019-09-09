75b0c812036673ba093a2eae25f8e0ca7b9b09bc
rain <634778311@qq.com>
2019-09-09 18:55:49 +0800

    Update test_future.dart

--- flutter_learn/lib/testFiles/test_future.dart
+++ flutter_learn/lib/testFiles/test_future.dart
@@ -1,6 +1,19 @@
+import 'dart:io';
+
 class TestFuture {
   
   startTest() {
+    _TestFutureFunction().testThen();
+    _TestFutureFunction().testWhenComplete();
+    _TestFutureFunction().testAwait();
+    _TestFutureFunction().testWait();
+  }
+}
+
+class _TestFutureFunction {
+
+  // then()回调中带参数，此参数为Future对象中包含的值
+  testThen() {
     getNumber1().then((aNum) {
       print('a = $aNum');
       return getNumber2(aNum);
@@ -8,15 +21,38 @@ class TestFuture {
       print('_ = $_');
       getResult(_);
     });
-
-
   }
-  
+
+  // whenComplete()中不带参数,只指定了顺序，此方法抛出异常后使用相当于finally
+  testWhenComplete() {
+    getNumber1().whenComplete((){
+      getNumber2(9).whenComplete((){
+        getResult(7).then((_){
+          print('TestWhenComplete');
+        });
+      });
+    });
+  }
+
+  // 使用async await
+  testAwait() async {
+    var number1 = await getNumber1();
+    var number2 = await getNumber2(number1);
+    print(await getResult(number2));
+  }
+
+  // wait()方法
+  testWait() {
+    Future.wait([getNumber1(), getNumber2(2), getResult(4)]).then((List result){
+      result.forEach((num) => print(num));
+    });
+  }
+
   Future<int> getNumber1() async {
     await print('getNumber1 10086');
     return 10086;
   }
-  
+
   Future getNumber2(int num) async {
     await print('getNumber2 ${num + 2}');
     return num + 2;
@@ -25,6 +61,4 @@ class TestFuture {
   Future getResult(int num) async {
     await print('getRusult');
   }
-
 }
-
