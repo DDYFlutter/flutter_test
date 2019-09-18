10d67d203ae37365bcb17e467e73f5f0dda3c837
dou <634778311@qq.com>
2019-09-19 00:07:47 +0800

    Update main_chats.dart

--- flutter_learn/lib/pageWidgets/chats/main_chats.dart
+++ flutter_learn/lib/pageWidgets/chats/main_chats.dart
@@ -6,6 +6,7 @@ import 'package:vector_math/vector_math_64.dart' as ddyVector;
 import 'package:flutter_learn/pageWidgets/qrcode/qrcode_scanner.dart';
 import 'dart:math';
 import 'package:flutter_learn/testFiles/test_listview.dart';
+import 'package:flutter_learn/pageWidgets/chats/main_left_drawer.dart';
 
 class ChatsPage extends StatefulWidget {
   String title;
@@ -19,6 +20,131 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+
+  final _ListItems = ['testListView', 'TestGridView'];
+
+  _buildOpenDrawerButton() {
+    return Builder(
+      builder: (context){
+        return IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+        );
+      },
+    );
+  }
+
+  _buildShowAlertButton() {
+    return IconButton(
+      icon: Icon(Icons.add_alert),
+      tooltip: '点我代表弹出提示--该按钮点击弹出',
+      onPressed: (){
+
+      },
+    );
+  }
+
+  _buildPopupMenuItem(IconData icon, String title, int menuID) {
+    return PopupMenuItem(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Icon(icon, color: Colors.blue,),
+          Padding(padding: EdgeInsets.all(5.0)),
+          Text(title),
+        ],
+      ),
+      value: menuID,
+    );
+  }
+
+  _popupMenuList() {
+    var items = new List<PopupMenuItem<int>>();
+    items.add(_buildPopupMenuItem(Icons.message, '发起聊天', 0));
+    items.add(_buildPopupMenuItem(Icons.group_add, '添加朋友', 1));
+    items.add(_buildPopupMenuItem(Icons.crop_free, '扫一扫', 2));
+    items.add(_buildPopupMenuItem(Icons.list, 'ListView', 3));
+    return items;
+  }
+
+  _buildPopupMenuButton() {
+    return PopupMenuButton<int>(
+      itemBuilder: (_) => _popupMenuList(),
+      onSelected: (index) {
+        switch(index) {
+          case 1: print('1'); break;
+          case 2: print('2'); break;
+          case 3: print('3'); break;
+          case 4: print('4'); break;
+        }
+      },
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text(widget.title ?? "首页"),
+      centerTitle: true,
+      leading: _buildOpenDrawerButton(),
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        _buildShowAlertButton(),
+        _buildPopupMenuButton(),
+      ],
+    );
+  }
+
+  _buildListView() {
+    return ListView.builder(
+      itemCount: _ListItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_ListItems[index]),
+          onTap: (){
+            switch(index) {
+              case 0: print('0'); break;
+              case 1: print('1'); break;
+              default: print('3'); break;
+            }
+          },
+        );
+      },
+    );
+  }
+
+  _buildLeftDrawer() {
+    return Drawer(
+      semanticLabel: '我',
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text('1'),
+            Text('2'),
+            Text('3'),
+            Text('4'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildListView(),
+      //drawer: _buildLeftDrawer(),
+      drawer: MainLeftDrawer.leftDrawer(),
+    );
+  }
+
+}
+
+class _ChatsPageState1 extends State<ChatsPage> {
   // 下拉菜单
   _buildPopupMenu(IconData icon, String title, String menuID) {
     return new PopupMenuItem<String>(
